c1067ba8d509682bde393718a3742c1a0a085f75
Bryan Cortes <bryan.cortes@ucr.ac.cr>
2022-05-25 11:45:53 -0400

    angular class

--- src/app/components/cliente/cliente.component.ts
+++ src/app/components/cliente/cliente.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
+import {ClienteService} from "../../shared/services/cliente.service";
+import {Cliente} from "../../shared/models/cliente.model";
 
 @Component({
   selector: 'app-cliente',
@@ -7,26 +10,78 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClienteComponent implements OnInit {
 
-  constructor() { }
+  titulo : string = '';
+  clientes = [new Cliente()];
+
+  constructor(private  srvCliente : ClienteService) {
+  }
+
+
 
   onNew(){
-    alert('New test');
+  this.titulo = 'Nuevo cliente';
   }
 
-  onEdit(){
-    alert('edit test');
+  onSave(){
+    Swal.fire({
+      position: 'center',
+      icon: 'success',
+      title: 'Cliente guardado',
+      showConfirmButton: false,
+      timer: 3000
+    })
   }
 
-  onDelete(){
-    alert('delete test');
+  onEdit(id : any){
+  this.titulo = 'Editar cliente' + id;
+  }
+
+  onDelete(id : any, nombre: string){
+    Swal.fire({
+      title: `Eliminar cliente ${id}?`,
+      text: nombre,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si, eliminar!',
+      cancelButtonText: 'Cancelar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire(
+          'Eliminado!',
+          'Tu archivo ha sido eliminado.',
+          'success'
+        )
+      }
+    })
   }
 
   onFilter(){
     alert('filter test');
   }
 
+  onPrint(){
+    alert('print test');
+  }
+
+  onClose(){
+    alert('close test');
+  }
+
 
   ngOnInit(): void {
+    this.srvCliente.filter({
+      idCliente : '',
+      nombre: '',
+      apellido1: '',
+      apellido2: ''
+    },1,10).subscribe(
+      data =>{
+        this.clientes = Object(data);
+        console.log(data)
+      }
+    )
   }
 
 }
---
+++ src/app/shared/services/cliente.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
+import {Observable, retry, throwError, catchError} from "rxjs";
+import {Cliente} from "../models/cliente.model";
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClienteService {
+
+  SRV : string = 'http://tallerbd';
+
+  constructor(private http : HttpClient) {
+
+  }
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type:' : 'Application/json'
+    })
+
+  }
+
+  filter(parametros : any, pag: number, lim: number) : Observable<Cliente>{
+    let params = new HttpParams;
+    for (const prop in parametros){
+      if(prop){
+        params = params.append(prop, parametros[prop])
+      }
+    }
+    return  this.http.get<Cliente>(`${this.SRV}/filtro/cliente/${pag}/${lim}`, {params:params})
+      .pipe(retry(1), catchError(this.handleError));
+  }
+  private handleError(error:any){
+    return throwError(()=>{
+      return error.status
+    })
+  }
+}
